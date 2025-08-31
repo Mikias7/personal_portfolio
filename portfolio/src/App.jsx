2f9c58191fb4f3beb97199c0d91e05437d840e36
Mikias7 <mikias7t@gmail.com>
2025-08-31 00:56:14 -0500

    initialized pages

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,3 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import AboutME from './pages/AboutMe';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
+
 import Navbar from "./components/Navabar"
 
 function App() {
@@ -5,6 +13,16 @@ function App() {
   return (
     <div>
       <Navbar />
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about_me" element={<AboutME />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/resume" element={<Resume />}/>
+          <Route path="/contact" element={<Contact />}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- portfolio/src/components/Navabar.jsx
+++ portfolio/src/components/Navabar.jsx
@@ -1,37 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 
 function Navbar() {
 
-    const [activeKey, setActiveKey] = useState("/home")
+    const [activeKey, setActiveKey] = useState("/")
 
     return (
-    <Nav
-        variant="underline"
-        defaultActiveKey={activeKey}
-        onSelect={(selectedKey) => setActiveKey(selectedKey)}
-    >
-        <Nav.Item>
-        <Nav.Link href="/home" eventKey="/home">Home</Nav.Link>
-        </Nav.Item>
+        <Nav
+            variant="underline"
+            activeKey={activeKey}
+            onSelect={(selectedKey) => setActiveKey(selectedKey)}
+        >
+            <Nav.Item>
+                <Nav.Link href="/" eventKey="/">Home</Nav.Link>
+            </Nav.Item>
 
-        <Nav.Item>
-        <Nav.Link href="/about_me" eventKey="/about_me">About Me</Nav.Link>
-        </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/about_me" eventKey="/about_me">About Me</Nav.Link>
+            </Nav.Item>
 
-        <Nav.Item>
-        <Nav.Link  href="/projects" eventKey="/projects">Projects</Nav.Link>
-        </Nav.Item>
+            <Nav.Item>
+                <Nav.Link  href="/projects" eventKey="/projects">Projects</Nav.Link>
+            </Nav.Item>
 
-        <Nav.Item>
-        <Nav.Link  href="/Resume" eventKey="/resume">Resume</Nav.Link>
-        </Nav.Item>
+            <Nav.Item>
+                <Nav.Link  href="/resume" eventKey="/resume">Resume</Nav.Link>
+            </Nav.Item>
 
-        <Nav.Item>
-        <Nav.Link  href="/Contact" eventKey="/contact">Contact</Nav.Link>
-        </Nav.Item>
+            <Nav.Item>
+                <Nav.Link  href="/contact" eventKey="/contact">Contact</Nav.Link>
+            </Nav.Item>
 
-    </Nav>
+        </Nav>
     );
 }
 
